346ca8493784329130665985f4e81f38b56073c0
Ahmadjon0125 <Ahmadjon0125@gmail.com>
2022-08-11 13:37:41 +0500

    className

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
 	return (
 		<header className="header">
 			<div className="container">
-				<div class="header__inner">
+				<div className="header__inner">
 					<h1 className="header__title">Where in the world?</h1>
 					<p className="header__text">Dark Mode</p>
 				</div>
